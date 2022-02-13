bd33ec18af477b65cb63fbd23528ca307ef114ba
Kevin F <Freymaurer@gmx.de>
2022-02-13 21:31:37 +0100

    add nfdi footer

--- src/Client/nfdi-webcomponents.fs
+++ src/Client/nfdi-webcomponents.fs
@@ -5,7 +5,10 @@ open Fable.Core.JS
 open Fable.Core.JsInterop
 
 let _ = import "Navbar" "@nfdi4plants/web-components"
+let _ = import "Footer" "@nfdi4plants/web-components"
 
 open Fable.React
 
 let nfdiNavbar props children = domEl "nfdi-navbar" props children
+
+let nfdiFooter props children = domEl "nfdi-footer" props children
